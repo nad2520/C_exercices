3267ac22b71fd9238c97c9651ab63d73de93a31e
nad2520 <nadinemahjoub464@gmail.com>
2025-03-26 15:48:20 +0100

    nbr d'elements+ d'une mat

---
+++ pointeurs/ex5_pt3_s2_a.c
@@ -0,0 +1,48 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int saisi(){
+    int n;
+    printf("n= ");
+    scanf("%d",&n);
+    return n;
+}
+void saisi_mat(int t[100][100],int n,int m){
+    int *p,i,j;
+    p=&t[0][0];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++)
+        { scanf(" %d" , p+i*m+j);}}
+}
+void afficher_mat(int t[100][100],int n,int m){
+    int *p,i,j;
+    p=&t[0][0];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++)
+        { printf(" %d\t" , *(p+i*m+j));
+        }printf("\n");}
+}
+int nbr_positif(int t[100][100],int n,int m){
+    int *p,i,j,s=0;
+    p=&t[0][0];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++)
+        { 
+            if(*(p+i*m+j)>0){
+                s++;
+            }
+        }
+    }
+    return s;
+}
+
+int main() {
+    int t[100][100],n,m;
+    n=saisi();
+    m=saisi();
+    saisi_mat(t, n,m);
+    afficher_mat(t, n,m);
+    int s=nbr_positif(t, n,m);
+    printf("\n");
+    printf("%d",s);
+    return 0;
+}
